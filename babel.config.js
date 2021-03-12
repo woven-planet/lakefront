2740e2c3a32fb9b87c67b9f6bb630f2e3ec835ae
Jonathan Burley <jonathanb@slalom.com>
2021-03-12 10:08:16 -0600

    Added @emotion babel plugin to project and babel.config as well as react to the dev dependencies.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = {
     presets: [
         '@babel/preset-env',
         '@babel/preset-react',
-        '@babel/preset-typescript',
+        '@babel/preset-typescript'
+    ],
+    plugins: [
+        '@emotion'
     ]
 };
