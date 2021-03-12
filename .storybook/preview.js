caddf29cd37639d288d0362df749c157008d80db
Jonathan Burley <jonathanb@slalom.com>
2021-03-12 12:54:40 -0600

    Add Storybook (#3)
    * Fix button tag
    
    * ci: version bump to v0.4.4-button.0
    
    * trigger new npm build via branch push
    
    * ci: version bump to v0.4.4-button.1
    
    * ci: version bump to v0.4.4-button.2
    
    * Added storybook.
    
    * Import button core component from lakehouse
    
    * ci: version bump to v0.4.4-button.3
    
    * Change version to match branch name.
    
    * Emotion config and theming.
    
    * Changed button props to reflect some coming changes.
    
    * Added newline.
    
    * Added to .gitignore, changed control for Button component color prop.
    
    * Added doc blocks to the button interface, replaced control for as with a few examples so the user can't break the page, expanded the table to show descriptions and default values, moved prop destructuring to the signature and added default values so storybook could properly read them.
    
    * Added onClick action and disabled table info.
    
    * Disabled onClick from the storybook summary table.
    
    * Changed baseUrl.
    
    * Updated package-lock.json.
    
    * ci: version bump to v0.4.6-storybook.0
    
    Co-authored-by: Mike Puzon <michael.puzon@tri.global>
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+import { addDecorator } from '@storybook/react';
+import { withThemesProvider } from 'storybook-addon-emotion-theme';
+import theme from 'src/styles/theme';
+
+const themes = [theme];
+addDecorator(withThemesProvider(themes));
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'default',
+    values: [
+      {
+        name: 'default',
+        value: theme.colors.white,
+      },
+      {
+        name: 'alternate',
+        value: theme.colors.gunpowder,
+      },
+    ],
+  },
+  controls: { expanded: true }
+}
---
+++ src/stories/Button/Button.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import Button from 'src/Button/Button';
+import { ButtonComponentProps, COLORS } from 'src/Button/buttonUtil';
+
+export default {
+    title: 'Example/Button',
+    component: Button,
+    argTypes: {
+        onClick: {
+            action: 'clicked',
+            table: {
+                disable: true
+            }
+        },
+        as: {
+            control: {
+                type: 'select',
+                options: ['a', 'button', 'div', 'span']
+            }
+        },
+        children: {
+            name: 'text (children)',
+            description: 'Usually text, but can accept other elements. If an icon is needed, use the icon prop.'
+        },
+        color: {
+            control: {
+                type: 'select',
+                options: [COLORS.PRIMARY, COLORS.SECONDARY, COLORS.DESTRUCTIVE],
+            },
+        },
+        disabled: {
+            control: 'boolean',
+            table: {
+                defaultValue: { summary: false },
+                type: { summary: 'boolean' }
+            },
+            description: 'HTML button element disabled prop'
+        }
+    }
+} as Meta;
+
+const Template: Story<ButtonComponentProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    children: 'Button',
+    color: COLORS.PRIMARY
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    children: 'Button',
+    color: COLORS.SECONDARY
+};
+
+export const Destructive = Template.bind({});
+Destructive.args = {
+    children: 'Button',
+    color: COLORS.DESTRUCTIVE
+};
