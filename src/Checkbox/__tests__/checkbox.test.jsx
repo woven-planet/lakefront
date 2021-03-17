77db875eb0f0ba58e9ed0a7589b3f626c652cefb
Jason Ransom <jason.ransom.ctr@tri.global>
2021-03-17 15:59:47 -0400

    Implemented base checkbox files.

---
+++ src/Checkbox/__tests__/checkbox.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Checkbox from '../Checkbox';
+
+describe('Checkbox', () => {
+    it.todo('renders un-checked checkbox and no label by default');
+    
+    it.todo('renders un-checked checkbox with label when label provided');
+    
+    it.todo('renders checked checkbox when "checked" prop is true');
+    
+    it.todo('triggers handler when checkbox is changed');
+    
+    it.todo('prevents handler trigger when disabled prop is true');
+    
+    it('FAILS UNTIL TEST ARE WRITTEN', () => {
+        expect('TODO').toBe('COMPLETE');
+    });
+});
