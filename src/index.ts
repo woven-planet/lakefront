6a1dcfec964ac55668d555d330d0c82c4c15ea7b
Mike Puzon <michael.puzon@tri.global>
2021-03-17 13:03:41 -0700

    Minor: Add text input and icon button component

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as Button } from './Button/Button';
+export { default as IconButton } from './Button/IconButton';
+export { default as Input } from './Input/Input';
 export { default as colors } from './styles/cloudColors';
