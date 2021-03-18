dec0cce929b52c107a48e72bc48bcffd7f2c277e
Jason Ransom <jason.ransom.ctr@tri.global>
2021-03-18 19:04:04 -0400

    Updated documentation to match current components.

--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -9,14 +9,40 @@ import { ReactComponent as Check } from "./assets/check.svg";
 import { ReactComponent as Indeterminate } from "./assets/indeterminate.svg";
 
 export interface CheckboxProps {
+  /**
+   * The initial value to control whether the checkbox should be checked or not.
+   */
   checked?: boolean;
+  /**
+   * The svg icon to display when checked is true. If left undefined, a check mark will be displayed.
+   */
   checkedIcon?: ReactElement<SVGElement>;
+  /**
+   * A display state in which it is unknown whether checked should be true or false.
+   */
   indeterminate?: boolean;
+  /**
+   * The (optional) text label for the checkbox.
+   */
   label?: string;
+  /**
+   * HTML input element disabled prop.
+   */
   disabled?: boolean;
+  /**
+   * The action that should be run when the checked state changes.
+   */
   onChange?: (event: object) => void;
 }
 
+/**
+ * Checkbox Component
+ *
+ * The Checkbox component takes in native checkbox props as well as its own CheckboxProps. The state is managed
+ * in this component, but could be managed outside (not recommended) by forcing rerenders with new initial props
+ * within the consuming app.
+ *
+ */
 const Checkbox: FC<CheckboxProps & ComponentPropsWithoutRef<"input">> = ({
   checked = false,
   checkedIcon,
--- src/stories/Checkbox/Checkbox.stories.tsx
+++ src/stories/Checkbox/Checkbox.stories.tsx
@@ -1,49 +1,36 @@
+import { ComponentPropsWithoutRef } from "react";
 import { Meta, Story } from '@storybook/react/types-6-0';
 
 import CheckboxComponent, { CheckboxProps } from 'src/Checkbox/Checkbox';
-import { ComponentPropsWithoutRef } from "react";
+import DocBlock from '.storybook/DocBlock';
 
 export default {
     title: 'Lakefront/Checkbox',
     component: CheckboxComponent,
     argTypes: {
         checked: {
-            control: false,
-            description: 'The initial value to control whether the checkbox should be checked or not.'
+            control: false
         },
         checkedIcon: {
             table: {
                 disable: true
-            },
-            description: 'The svg icon to display when checked is true. If left undefined, a check mark will be displayed.'
-        },
-        indeterminate: {
-            control: 'boolean',
-            description: 'A display state in which it is unknown whether checked should be true or false.'
-        },
-        label: {
-            control: 'text',
-            description: 'The (optional) text label for the checkbox.',
-        },
-        disabled: {
-            control: 'boolean',
-            table: {
-                defaultValue: { summary: false },
-                type: { summary: 'boolean' }
-            },
-            description: 'HTML input element disabled prop.'
+            }
         },
         onChange: {
-            control: false,
-            description: 'The action that should be run when the checked state changes.'
+            control: false
         },
-        // Included to properly trigger storybook action console
+        // onClick included to properly trigger storybook action console
         onClick: {
-            action: 'clicked',
+            action: 'changed',
             table: {
                 disable: true
             }
         },
+    },
+    parameters: {
+        docs: {
+            page: DocBlock
+        }
     }
 } as Meta;
 
