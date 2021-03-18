586f8fb08e78bb719c93e91b23c5c2204d0b9dad
Jonathan Burley <jonathanb@slalom.com>
2021-03-18 15:17:07 -0500

    Made decorator component more readable.

--- .storybook/GlobalStyleStorybookDecorator.tsx
+++ .storybook/GlobalStyleStorybookDecorator.tsx
@@ -1,7 +1,11 @@
 import GlobalStyles from '../src/styles/GlobalStyles';
 
 const GlobalStyleStorybookDecorator = (storyFn: any) => {
-    return <GlobalStyles>{ storyFn() }</GlobalStyles>
+    return (
+        <GlobalStyles>
+            {storyFn()}
+        </GlobalStyles>
+    );
 };
 
 export default GlobalStyleStorybookDecorator;
