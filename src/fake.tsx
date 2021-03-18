62649630eaa3b418d32b02d9a8fa8f1f2ec825ec
Mike Puzon <michael.puzon@tri.global>
2021-03-18 13:56:41 -0700

    Test our pre-release workflow

---
+++ src/fake.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Fake: FC= ({ ...props }) => {
+    return (
+        <div>
+            test div
+        </div>
+    );
+};
+
+export default Fake;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button/Button';
 export { default as IconButton } from './Button/IconButton';
 export { default as Input } from './Input/Input';
+export { default as Fake } from './fake';
 export { default as colors } from './styles/cloudColors';
