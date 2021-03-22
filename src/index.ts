1196c1b79a23ec73bcfd9a623f90a3e0f4790058
Jonathan Burley <jonathanb@slalom.com>
2021-03-22 11:11:34 -0500

    Minor fixes (#14)
    * Implemented base checkbox files.
    
    * Added svgs and updated state changes.
    
    * Added disabled styles.
    
    * Updated checkbox label positioning and component documentation.
    
    * Completed writing the majority of tests.
    
    * Updated documentation to match current components.
    
    * Finished tests and added cursor disable when applicable.
    
    * Changed onChange typing for Checkbox component, changed quotes to apostrophes, removed onClick from code snippets for Checkbox stories.
    
    * Exporting theme for use externally.
    
    * Added Emotion docs to README.
    
    * Bumped version
    
    * Bump version
    
    * Added @emotion libraries to dependencies.
    
    * ci: version bump to v0.5.6
    
    Co-authored-by: Jason Ransom <jason.ransom.ctr@tri.global>
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button/Button';
 export { default as IconButton } from './Button/IconButton';
 export { default as Input } from './Input/Input';
 export { default as colors } from './styles/cloudColors';
+export { default as theme } from './styles/theme';
