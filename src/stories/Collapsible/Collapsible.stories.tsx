10c1395aab99fe24bb7386533bdd8c18a1b32614
Jason Ransom <jason.ransom.ctr@tri.global>
2021-03-29 12:59:25 -0400

    Added collapsible component, styling, and story.

---
+++ src/stories/Collapsible/Collapsible.stories.tsx
@@ -0,0 +1,57 @@
+import { ComponentPropsWithoutRef } from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import CollapsibleComponent, { CollapsibleProps } from 'src/Collapsible/Collapsible';
+import DocBlock from '.storybook/DocBlock';
+
+export default {
+    title: 'Lakefront/Collapsible',
+    component: CollapsibleComponent,
+    argTypes: {
+        title: {
+            control: {
+                type: 'text'
+            }
+        },
+        subtitle: {
+            control: {
+                type: 'text'
+            }
+        },
+        onChange: {
+            action: 'changed',
+            table: {
+                disable: true
+            }
+        },
+        children: {
+            table: {
+                disable: true
+            }
+        }
+    },
+    parameters: {
+        docs: {
+            page: DocBlock
+        }
+    }
+} as Meta;
+
+const Template: Story<CollapsibleProps & ComponentPropsWithoutRef<"div">> = (
+  args
+) => (
+  <CollapsibleComponent {...args}>
+    <div>
+      <p>You can have any content here.</p>
+      <p>It could be text, images, graphs, etc.</p>
+      <p>You could also just leave it blank.</p>
+    </div>
+  </CollapsibleComponent>
+);
+
+export const Collapsible = Template.bind({});
+Collapsible.args = {
+  expanded: true,
+  title: "Collapsible Component",
+  subtitle: <div>Additional Info | <strong>100</strong> Count | Updated <strong>Today</strong></div>
+};
