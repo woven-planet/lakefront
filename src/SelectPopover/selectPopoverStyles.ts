41925870c4623565fe1840aa6c0ea88d13d42dc9
Jonathan Burley <jonathanb@slalom.com>
2021-04-06 15:11:56 -0500

    Alphabetized the storybook links in the README, added some styles to the select popover to make sure they don't accidentally get overwritten by page styling.

--- src/SelectPopover/selectPopoverStyles.ts
+++ src/SelectPopover/selectPopoverStyles.ts
@@ -9,23 +9,27 @@ export const StyledSelectPopover = styled.div(({ theme }) => ({
     backgroundColor: theme?.colors?.white,
     border: `1px solid ${theme?.colors?.cinder}`,
     borderRadius: 4,
+    display: 'flex',
+    flexDirection: 'column',
     overflow: 'hidden',
     position: 'absolute',
     top: '100%',
     left: '50%',
-    transform: 'translateX(-55%)'
+    transform: 'translateX(-55%)',
+    zIndex: 2
 }));
 
 export const SelectPopoverItem = styled.div(({ theme }) => ({
     alignItems: 'center',
+    backgroundColor: theme?.colors?.white,
     color: theme?.colors?.storm,
     cursor: 'pointer',
-    display: 'flex',
     fontSize: 16,
     height: 40,
     minWidth: 160,
     padding: '10px 12px',
     userSelect: 'none',
+    zIndex: 2,
     ':hover': {
         backgroundColor: theme?.colors?.mercury
     }
