d94637089b37a1d5fb5c1141322255445a1e49ad
zach-jones-slalom <80778912+zach-jones-slalom@users.noreply.github.com>
2021-04-27 12:03:43 -0500

    Radio group unit tests (#50)
    * added unit tests for radio group
    
    * ci: version bump to v0.16.1
    
    Co-authored-by: Zach Jones <zach.jones.ctr@tri.global>
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

---
+++ src/RadioGroup/__tests__/radioGroup.test.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import RadioGroup from "../../RadioGroup/RadioGroup";
+
+const name = 'Alphabet';
+const options = [
+    {label: 'A', value: 'A'},
+    {label: 'B', value: 'B'},
+    {label: 'C', value: 'C'}
+];
+
+describe('RadioGroup', () => {
+    describe('general rendering', () => {
+        it('renders the radio buttons', () => {
+            const { container } = render(
+                <RadioGroup name={name} options={options} />
+            );
+
+            const radioButtons = container.querySelectorAll('input');
+
+            expect(radioButtons).toHaveLength(3);
+        });
+
+        it('should render the buttons disabled when disabled is true', () => {
+            const { container } = render(
+                <RadioGroup name={name} options={options} disabled={true} />
+            );
+
+            const radioButtons = container.querySelectorAll('input');
+
+            expect(radioButtons[0]).toHaveAttribute('disabled');
+            expect(radioButtons[1]).toHaveAttribute('disabled');
+            expect(radioButtons[2]).toHaveAttribute('disabled');
+        });
+    });
+});
