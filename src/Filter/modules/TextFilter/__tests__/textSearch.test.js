f8b299c8d8942aa3c922f46ddffedc7f3360654c
Jason Ransom <jason.ransom.ctr@tri.global>
2021-07-02 10:13:48 -0400

    Added TextSearch tests.

--- src/Filter/modules/TextFilter/__tests__/textSearch.test.js
+++ src/Filter/modules/TextFilter/__tests__/textSearch.test.js
@@ -1,32 +1,32 @@
 import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
 import TextSearch from '../TextSearch';
 
 describe('<TextSearch />', () => {
-    // it('renders input text', () => {
-    //     const wrapper = shallow(<TextSearch />);
-    //     expect(wrapper.find('input').length).toBe(1);
-    // });
+    it('renders input text', () => {
+        const { getByRole } = render(<TextSearch />);
 
-    // it('onChange callback does not fire on input change', () => {
-    //     const changeCallback = jest.fn();
-    //     const wrapper = shallow(<TextSearch onChange={changeCallback} />);
-    //     wrapper.find('input').simulate('change', { target: { value: 'asdf' } });
-    //     expect(changeCallback).not.toBeCalled();
-    // });
+        getByRole('textbox');
+    });
 
-    // it('onChange callback fires on blur change', () => {
-    //     const submitCallback = jest.fn();
-    //     const wrapper = shallow(<TextSearch onChange={submitCallback} />);
-    //     wrapper.find('input').simulate('blur', { target: { value: 'asdf' } });
-    //     expect(submitCallback).toBeCalled();
-    //     expect(submitCallback).toBeCalledWith('asdf');
-    // });
+    it('onChange callback does not fire on input change', () => {
+        const changeCallback = jest.fn();
+        const { getByRole } = render(<TextSearch onChange={changeCallback} />);
+        fireEvent.change(getByRole('textbox'), { target: { value: 'asdf' } });
+        expect(changeCallback).not.toBeCalled();
+    });
 
-    // it('onChange callback fires on enter key press', () => {
-    //     const submitCallback = jest.fn();
-    //     const wrapper = shallow(<TextSearch onChange={submitCallback} />);
-    //     wrapper.find('input').simulate('keyPress', { key: 'Enter', target: { value: 'asdf' } });
-    //     expect(submitCallback).toBeCalled();
-    //     expect(submitCallback).toBeCalledWith('asdf');
-    // });
+    it('onChange callback fires on blur change', () => {
+        const changeCallback = jest.fn();
+        const { getByRole } = render(<TextSearch onChange={changeCallback} />);
+        fireEvent.blur(getByRole('textbox'), { target: { value: 'asdf' } });
+        expect(changeCallback).toBeCalledWith('asdf');
+    });
+
+    it('onChange callback fires on enter key press', () => {
+        const changeCallback = jest.fn();
+        const { getByRole } = render(<TextSearch onChange={changeCallback} />);
+        fireEvent.keyPress(getByRole('textbox'), { key: 'Enter', charCode: 13, target: { value: 'asdf' } });
+        expect(changeCallback).toBeCalledWith('asdf');
+    });
 });
--- src/Filter/modules/TextFilter/textSearchStyles.ts
+++ src/Filter/modules/TextFilter/textSearchStyles.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import Input from 'src/Input/Input';
+import Input from '../../../Input/Input';
 
 export const StyledInput = styled(Input)({
     width: '100%'
