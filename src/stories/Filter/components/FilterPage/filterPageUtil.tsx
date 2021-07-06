2f94655f690696fe2cdd9fcab968d9f9a000944d
Jason Ransom <jason.ransom.ctr@tri.global>
2021-07-06 11:48:59 -0400

    Updated radio filter chip label on All filters story.

--- src/stories/Filter/components/FilterPage/filterPageUtil.tsx
+++ src/stories/Filter/components/FilterPage/filterPageUtil.tsx
@@ -20,13 +20,18 @@ const RADIO_FILTER_OPTIONS = [
 ];
 
 export const FILTERS = {
-    radioFilter: RadioFilter({
-        label: 'Radio Filter',
-        defaultValue: '',
-        initialValue: RADIO_FILTER_OPTIONS[0].value,
-        options: RADIO_FILTER_OPTIONS,
-        description: 'RadioFilter is a radio group control meant to single select a value.'
-    })
+    radioFilter: RadioFilter(
+        {
+            label: 'Radio Filter',
+            defaultValue: '',
+            initialValue: RADIO_FILTER_OPTIONS[0].value,
+            options: RADIO_FILTER_OPTIONS,
+            description: 'RadioFilter is a radio group control meant to single select a value.'
+        },
+        {
+            getFilterBarLabel: (value: string) => `Radio Filter: ${value}`
+        }
+    )
 };
 
 export const LOCATION = {
