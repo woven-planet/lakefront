7aad80e6849d4488c0a318d3885e5fbe46f0a9cc
Jason Ransom <jason.ransom.ctr@tri.global>
2021-07-06 15:43:32 -0400

    Added remaining radio filter tests.

--- src/Filter/modules/RadioFilter/__tests__/radioFilter.test.js
+++ src/Filter/modules/RadioFilter/__tests__/radioFilter.test.js
@@ -2,6 +2,25 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import RadioFilter from '../RadioFilter';
 
+const RADIO_FILTER_OPTIONS = [
+    {
+        label: 'North',
+        value: 'north'
+    },
+    {
+        label: 'East',
+        value: 'east'
+    },
+    {
+        label: 'South',
+        value: 'south'
+    },
+    {
+        label: 'West',
+        value: 'west'
+    }
+];
+
 describe('RadioFilter', () => {
     it('returns the proper label', () => {
         const label = 'label';
@@ -43,123 +62,130 @@ describe('RadioFilter', () => {
         });
     });
 
-    // describe('getBrowserQueryUrlValue', () => {
-    //     const { getBrowserQueryUrlValue } = TextFilter('', '');
+    describe('getBrowserQueryUrlValue', () => {
+        const { getBrowserQueryUrlValue } = RadioFilter({}, {});
 
-    //     it('returns the value provided', () => {
-    //         expect(getBrowserQueryUrlValue('a')).toBe('a');
-    //         expect(getBrowserQueryUrlValue(1)).toBe(1);
-    //         expect(getBrowserQueryUrlValue()).toBeUndefined();
-    //     });
-    // });
+        it('returns the value provided', () => {
+            expect(getBrowserQueryUrlValue('a')).toBe('a');
+            expect(getBrowserQueryUrlValue(1)).toBe(1);
+            expect(getBrowserQueryUrlValue()).toBeUndefined();
+        });
+    });
 
-    // describe('getDefaultFilterValue', () => {
-    //     const { getDefaultFilterValue } = TextFilter('', '');
+    describe('getDefaultFilterValue', () => {
+        const defaultValue = 'default';
+        const { getDefaultFilterValue } = RadioFilter({ defaultValue }, {});
 
-    //     it('returns an empty string', () => {
-    //         expect(getDefaultFilterValue('a')).toBe('');
-    //         expect(getDefaultFilterValue(1)).toBe('');
-    //         expect(getDefaultFilterValue()).toBe('');
-    //     });
-    // });
+        it('returns a provided default value', () => {
+            expect(getDefaultFilterValue('a')).toBe(defaultValue);
+            expect(getDefaultFilterValue(1)).toBe(defaultValue);
+            expect(getDefaultFilterValue()).toBe(defaultValue);
+        });
+    });
 
-    // describe('isDefaultFilterValue', () => {
-    //     const { isDefaultFilterValue } = TextFilter('', '');
+    describe('isDefaultFilterValue', () => {
+        const defaultValue = 'default';
+        const { isDefaultFilterValue } = RadioFilter({ defaultValue }, {});
 
-    //     it('returns true if value is equal to empty string', () => {
-    //         expect(isDefaultFilterValue('')).toBe(true);
-    //         expect(isDefaultFilterValue('a')).toBe(false);
-    //         expect(isDefaultFilterValue()).toBe(false);
-    //     });
-    // });
+        it(`returns true if value is equal to ${defaultValue}`, () => {
+            expect(isDefaultFilterValue(defaultValue)).toBe(true);
+            expect(isDefaultFilterValue('a')).toBe(false);
+            expect(isDefaultFilterValue()).toBe(false);
+        });
+    });
 
-    // describe('getFilterBarLabel', () => {
-    //     const { getFilterBarLabel } = TextFilter('', '');
+    describe('getFilterBarLabel', () => {
+        const { getFilterBarLabel } = RadioFilter({ options: RADIO_FILTER_OPTIONS }, {});
 
-    //     it('returns the value provided', () => {
-    //         expect(getFilterBarLabel('a')).toBe('a');
-    //         expect(getFilterBarLabel(1)).toBe(1);
-    //         expect(getFilterBarLabel()).toBeUndefined();
-    //     });
-    // });
+        it('returns the associated label if value exists in options list', () => {
+            expect(getFilterBarLabel(RADIO_FILTER_OPTIONS[0].value)).toBe(RADIO_FILTER_OPTIONS[0].label);
+            expect(getFilterBarLabel(RADIO_FILTER_OPTIONS[1].value)).toBe(RADIO_FILTER_OPTIONS[1].label);
+        });
 
-    // describe('parseInitialFilterValue', () => {
-    //     const { parseInitialFilterValue } = TextFilter('', '');
+        it('returns empty string if value does not exist in options list', () => {
+            expect(getFilterBarLabel('a')).toBe('');
+            expect(getFilterBarLabel('west2')).toBe('');
+            expect(getFilterBarLabel()).toBe('');
+        });
+    });
 
-    //     it('returns value if value is truthy', () => {
-    //         expect(parseInitialFilterValue('a')).toBe('a');
-    //         expect(parseInitialFilterValue(1)).toBe(1);
-    //     });
+    describe('parseInitialFilterValue', () => {
+        const { parseInitialFilterValue } = RadioFilter({ initialValue: '' }, {});
 
-    //     it('returns empty string if value is falsy', () => {
-    //         expect(parseInitialFilterValue('')).toBe('');
-    //         expect(parseInitialFilterValue(null)).toBe('');
-    //         expect(parseInitialFilterValue(0)).toBe('');
-    //         expect(parseInitialFilterValue()).toBe('');
-    //     });
-    // });
+        it('returns value if value is truthy', () => {
+            expect(parseInitialFilterValue('a')).toBe('a');
+            expect(parseInitialFilterValue(1)).toBe(1);
+        });
 
-    // describe('renderComponent', () => {
-    //     const { renderComponent } = TextFilter('', '');
+        it('returns initialValue if value is falsy', () => {
+            expect(parseInitialFilterValue('')).toBe('');
+            expect(parseInitialFilterValue(null)).toBe('');
+            expect(parseInitialFilterValue(0)).toBe('');
+            expect(parseInitialFilterValue()).toBe('');
+        });
+    });
 
-    //     it('returns the expected component', () => {
-    //         const update = jest.fn();
-    //         const { getByRole } = render(<div>{renderComponent({ name: 'name', value: '1', update })}</div>);
-    //         expect(getByRole('textbox')).toHaveValue('1');
-    //         fireEvent.blur(getByRole('textbox'), { target: { value: 'asdf' } });
-    //         expect(update).toBeCalledWith('asdf');
-    //     });
-    // });
+    describe('renderComponent', () => {
+        const { renderComponent } = RadioFilter({ options: RADIO_FILTER_OPTIONS }, {});
 
-    // describe('textFilterOptions', () => {
-    //     const filterModuleKeys = [
-    //         'label',
-    //         'description',
-    //         'getApiQueryUrl',
-    //         'getApiPostBody',
-    //         'getBrowserQueryUrlValue',
-    //         'getDefaultFilterValue',
-    //         'isDefaultFilterValue',
-    //         'getFilterBarLabel',
-    //         'parseInitialFilterValue',
-    //         'renderComponent'
-    //     ];
+        it('returns the expected component', () => {
+            const update = () => null;
+            const { container } = render(<div>{renderComponent({ name: 'name', value: '1', update })}</div>);
+            expect(container.querySelectorAll('input')).toHaveLength(4);
+        });
+    });
 
-    //     const {
-    //         label,
-    //         description,
-    //         getApiQueryUrl,
-    //         getApiPostBody,
-    //         getBrowserQueryUrlValue,
-    //         getDefaultFilterValue,
-    //         isDefaultFilterValue,
-    //         getFilterBarLabel,
-    //         parseInitialFilterValue,
-    //         renderComponent
-    //     } = TextFilter(
-    //         'label',
-    //         'description',
-    //         filterModuleKeys.reduce((acc, k) => ({ ...acc, [k]: () => null }), {})
-    //     );
+    describe('radioFilterOptions', () => {
+        const filterModuleKeys = [
+            'label',
+            'description',
+            'getApiQueryUrl',
+            'getApiPostBody',
+            'getBrowserQueryUrlValue',
+            'getDefaultFilterValue',
+            'isDefaultFilterValue',
+            'getFilterBarLabel',
+            'parseInitialFilterValue',
+            'renderComponent'
+        ];
 
-    //     it('does not override label and description', () => {
-    //         expect(label).toBe('label');
-    //         expect(description).toBe('description');
-    //     });
+        const {
+            label,
+            description,
+            getApiQueryUrl,
+            getApiPostBody,
+            getBrowserQueryUrlValue,
+            getDefaultFilterValue,
+            isDefaultFilterValue,
+            getFilterBarLabel,
+            parseInitialFilterValue,
+            renderComponent
+        } = RadioFilter(
+            {
+                label: 'label',
+                description: 'description'
+            },
+            filterModuleKeys.reduce((acc, k) => ({ ...acc, [k]: () => null }), {})
+        );
 
-    //     it('overrides default methods', () => {
-    //         expect(getApiQueryUrl('a', 'b')).toBeNull();
-    //         expect(getApiPostBody('a', 'b')).toBeNull();
-    //         expect(getBrowserQueryUrlValue('a')).toBeNull();
-    //         expect(getDefaultFilterValue('a')).toBeNull();
-    //         expect(isDefaultFilterValue('a')).toBeNull();
-    //         expect(getFilterBarLabel('a')).toBeNull();
-    //         expect(parseInitialFilterValue('a')).toBeNull();
+        it('does not override label and description', () => {
+            expect(label).toBe('label');
+            expect(description).toBe('description');
+        });
 
-    //         const { queryByRole } = render(
-    //             <div>{renderComponent({ name: 'name', value: '1', update: () => null })}</div>
-    //         );
-    //         expect(queryByRole('textbox')).not.toBeInTheDocument();
-    //     });
-    // });
+        it('overrides default methods', () => {
+            expect(getApiQueryUrl('a', 'b')).toBeNull();
+            expect(getApiPostBody('a', 'b')).toBeNull();
+            expect(getBrowserQueryUrlValue('a')).toBeNull();
+            expect(getDefaultFilterValue('a')).toBeNull();
+            expect(isDefaultFilterValue('a')).toBeNull();
+            expect(getFilterBarLabel('a')).toBeNull();
+            expect(parseInitialFilterValue('a')).toBeNull();
+
+            const { queryByRole } = render(
+                <div>{renderComponent({ name: 'name', value: '1', update: () => null })}</div>
+            );
+            expect(queryByRole('textbox')).not.toBeInTheDocument();
+        });
+    });
 });
