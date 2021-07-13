23f0627743b9d4b96973905c30708db8b67d9a76
Jason Ransom <jason.ransom.ctr@tri.global>
2021-07-13 12:44:14 -0400

    Updated styles to match single select filter.

--- src/Filter/modules/MultiSelectFilter/multiSelectStyles.ts
+++ src/Filter/modules/MultiSelectFilter/multiSelectStyles.ts
@@ -1,20 +1,32 @@
 import { GroupTypeBase, Styles } from 'react-select';
 import { MultiSelectOption } from './MultiSelect';
+import { lightenDarkenColor } from 'src/styles/stylesUtil';
+
+const DARKEN_LEAST = -10;
 
 export const MULTI_SELECT_STYLES: Partial<Styles<MultiSelectOption, true, GroupTypeBase<MultiSelectOption>>> = {
     control: (styles, state) => ({
         ...styles,
         backgroundColor: '#ffffff',
-        borderColor: state.theme.colors.storm,
-        boxShadow: state.isFocused ? `0 0 0 1px ${state.theme.colors.storm}` : 'none',
-        borderRadius: 2,
+        borderRadius: 4,
+        border: `1px solid ${state.theme.colors.mercury}`,
+        boxShadow: 'inset 0 1px 2px 0 rgb(0 0 0 / 20%), inset 0 0 0 1px rgb(0 0 0 / 20%)',
+        ...(state.isFocused && {
+            border: `1px solid ${state.theme.colors.storm}`,
+            outline: 0
+        }),
         ':hover': {
-            borderColor: state.theme.colors.storm
+            backgroundColor: lightenDarkenColor(state.theme?.colors?.white, DARKEN_LEAST),
+            border: `1px solid ${state.theme.colors.storm}`,
+            outline: 0
         }
     }),
     multiValue: (styles, state) => ({
         ...styles,
-        backgroundColor: state.theme.colors.selago
+        backgroundColor: state.theme.colors.selago,
+        ':hover': {
+            backgroundColor: lightenDarkenColor(state.theme.colors.selago, DARKEN_LEAST)
+        }
     }),
     option: (styles, state) => ({
         ...styles,
