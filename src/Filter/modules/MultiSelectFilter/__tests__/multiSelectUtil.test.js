8279af3a6093c2bfd81b4f5b406f1a66da1ba8f9
Jason Ransom <jason.ransom.ctr@tri.global>
2021-08-06 10:04:04 -0400

    Added tests for parseItems function.

--- src/Filter/modules/MultiSelectFilter/__tests__/multiSelectUtil.test.js
+++ src/Filter/modules/MultiSelectFilter/__tests__/multiSelectUtil.test.js
@@ -1,4 +1,4 @@
-import { createOption, createUniqueOptions } from '../multiSelectUtil';
+import { createOption, createUniqueOptions, parseItems } from '../multiSelectUtil';
 
 describe('createOption', () => {
     it('returns an object with the correct label and value.', () => {
@@ -34,5 +34,35 @@ describe('createUniqueOptions', () => {
         ]);
 
         expect(createdOptions).toHaveLength(2);
+    });
+});
+
+describe('parseItems', () => {
+    const newLineItems = 'a\nb\nc';
+    const delimiter = '\n';
+
+    it('returns array of single item if parseMultiValue is undefined', () => {
+        expect(parseItems(newLineItems)).toMatchObject([newLineItems]);
+    });
+
+    it('returns array of single item if parseMultiValue.enabled is falsy', () => {
+        expect(parseItems(newLineItems, { delimiter })).toMatchObject([newLineItems]);
+        expect(parseItems(newLineItems, { enabled: false, delimiter })).toMatchObject([newLineItems]);
+    });
+
+    it('returns array of single item if item does not include delimiter', () => {
+        const newCommaItems = 'a,b,c';
+        expect(parseItems(newCommaItems, { enabled: true, delimiter })).toMatchObject([newCommaItems]);
+    });
+
+    it('returns list of items parsed by specified delimiter', () => {
+        expect(parseItems(newLineItems, { enabled: true, delimiter })).toMatchObject(newLineItems.split(delimiter));
+    });
+
+    it('removes items from parsed length that are only whitespace', () => {
+        const itemsWithEmptyEntries = 'a\nb\n \n   \n\n\nc';
+
+        expect(parseItems(itemsWithEmptyEntries, { enabled: true, delimiter })).toMatchObject(['a', 'b', 'c']);
+        expect(parseItems(itemsWithEmptyEntries, { enabled: true, delimiter })).toHaveLength(3);
     });
 });
