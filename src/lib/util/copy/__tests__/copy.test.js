8bb36236c787532f5fe2525c3541a60af62ca106
Jason Ransom <jason.ransom.ctr@tri.global>
2021-08-24 11:36:33 -0400

    Added tests for copy util function.

---
+++ src/lib/util/copy/__tests__/copy.test.js
@@ -0,0 +1,36 @@
+import copy from '../copy';
+
+describe('copy', () => {
+    it('copies the provided text to the clipboard', () => {
+        const writeText = jest.fn();
+
+        global.navigator.clipboard = {
+            writeText
+        };
+
+        copy('text');
+
+        expect(writeText).toBeCalledTimes(1);
+        expect(writeText).toHaveBeenCalledWith('text');
+    });
+
+    it('returns true on successful copy', () => {
+        global.navigator.clipboard = {
+            writeText: () => null
+        };
+
+        copy('text');
+
+        expect(copy('text')).toBe(true);
+    });
+
+    it('returns false on failed copy', () => {
+        global.navigator.clipboard = {
+            writeText: () => { throw new Error('error') }
+        };
+
+        copy('text');
+
+        expect(copy('text')).toBe(false);
+    });
+});
