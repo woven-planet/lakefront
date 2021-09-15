e3ea1d2803ee7f87b051008237df8264a4f5c2c1
nehal-bhandari-ctr <nehal.bhandari.ctr@tri.global>
2021-09-15 13:15:48 -0400

    Feature: Added new component Page and Header

--- src/Page/Header.tsx
+++ src/Page/Header.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from '@emotion/react';
 import { StyledHeader } from './pageStyles';
 import theme from 'src/styles/theme';
 
-/**
+ /**
  * Header Component
  * 
  * The Header component is used to render the header.
