7e3ea69334af9d8263eb1eedb51ad0c7e91f9e62
nehal-bhandari-ctr <nehal.bhandari.ctr@tri.global>
2021-11-01 22:05:04 -0400

    Added Circular Progress Component and the story for Circular Progress

---
+++ src/Progress/CircularProgress.tsx
@@ -0,0 +1,100 @@
+import { FC, ReactNode, useRef, useEffect } from 'react';
+import { ThemeProvider } from '@emotion/react';
+import customTheme from 'src/styles/theme';
+import { arc as d3arc, pie as d3pie } from 'd3-shape';
+import { event, select } from 'd3-selection';
+import { CircularProgressStyle, CenterTextStyle } from './circularProgressStyles';
+export interface CircularProgressProps {
+    width: number;
+    text?: ReactNode;
+    data: {
+        label: string;
+        value: number;
+        tooltip?: boolean;
+        key?: string;
+    }[];
+    theme: {
+        [key: string]: {
+            bgColor: string;
+            fgColor: string;
+        };
+    };
+}
+
+
+const CircularProgress: FC<CircularProgressProps> = ({ width, text, data, theme }) => {
+
+    const svgRef = useRef<SVGSVGElement>(null);
+    const htmlRef = useRef<HTMLDivElement>(null);
+    const radius = width / 2;
+    const circleRadius = radius - 5;
+
+    const drawCircularProgress = () => {
+        if (!svgRef.current) {
+            return;
+        }
+
+        const svg = select(svgRef.current);
+        const html = select(htmlRef.current);
+
+        // for tooltips later
+        const div = html
+            .select('div')
+            .attr('class', 'tooltip')
+            .style('opacity', 0);
+
+        const g = svg.select('g');
+        const radius = width / 2;
+
+        const arc = d3arc().outerRadius(radius).innerRadius(radius - 10).padAngle(0);
+
+        const paths: any = g.selectAll('path').data(d3pie()(data.map(d => d.value)));
+        paths
+            .enter()
+            .append('path')
+            .merge(paths)
+            // draw the arcs
+            .attr('d', arc)
+            .attr('fill', (d: any, i: any) => {
+                const key = data[i].key || data[i].label;
+                return theme[key] ? theme[key].bgColor : 'transparent';
+            })
+            // have a mouse tooltip effect
+            .on('mouseover', function (d: any) {
+                if (d.data.tooltip === undefined || d.data.tooltip === true) {
+                    div.transition()
+                        .duration(200)
+                        .style('opacity', 0.9);
+                    div.html(`${d.data.label}: ${d.value}`)
+                        .style('left', `${event.pageX}px`)
+                        .style('top', `${event.pageY - 28}px`);
+                }
+            })
+            .on('mouseout', function (d: any) {
+                div.transition()
+                    .duration(500)
+                    .style('opacity', 0);
+            });
+
+        paths.exit().remove();
+    };
+
+    useEffect(() => {
+        drawCircularProgress();
+    });
+
+    return (
+        <ThemeProvider theme={customTheme}>
+            <CircularProgressStyle width={width} ref={htmlRef}>
+                <svg width={width} height={width} ref={svgRef}>
+                    <circle cx={radius} cy={radius} r={circleRadius} fill="transparent" stroke={customTheme?.colors.selago} />
+                    <g transform={`translate(${width / 2} ,  ${width / 2})`} />
+                </svg>
+                <CenterTextStyle>{text}</CenterTextStyle>
+                <div />
+            </CircularProgressStyle>
+        </ThemeProvider>
+    );
+}
+
+export default CircularProgress;
