0b83d321350ff9f5fe9ce7eaaa2480c6e8ec7a64
nehal-bhandari-ctr <nehal.bhandari.ctr@tri.global>
2021-12-03 10:32:29 -0500

    Fixed the tab styling issue

--- src/Tabs/tabStyles.ts
+++ src/Tabs/tabStyles.ts
@@ -3,17 +3,15 @@ import styled from '@emotion/styled';
 export const TabBar = styled.div<any>(({ theme }) => ({
     display: 'flex',
     flexFlow: 'row nowrap',
-    backgroundColor: theme?.colors?.white
+    backgroundColor: theme?.colors?.white,
+    marginLeft: '-2.5rem'
 }));
 
 
 export const TabStyle = styled.span<any>(({ theme, isSelected }) => ({
-    margin: '2.5rem',
     color: theme?.colors?.pavement,
-    padding: '0.5rem 0 0.625 0',
-    '& + .tab': {
-        marginLeft: '2.5rem'
-    },
+    padding: '0.5rem 0 0.625rem 0',
+    marginLeft: '2.5rem',
     cursor: 'pointer',
     ...(isSelected) && {
         color: theme?.colors?.cinder,
