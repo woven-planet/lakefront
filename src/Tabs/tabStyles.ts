7cc1b450c17dcae46a2407920afb3981e5c87daf
nehal-bhandari-ctr <nehal.bhandari.ctr@tri.global>
2021-12-06 10:58:57 -0500

    Fixed Tabs styling issue

--- src/Tabs/tabStyles.ts
+++ src/Tabs/tabStyles.ts
@@ -3,16 +3,17 @@ import styled from '@emotion/styled';
 export const TabBar = styled.div<any>(({ theme }) => ({
     display: 'flex',
     flexFlow: 'row nowrap',
-    backgroundColor: theme?.colors?.white,
-    marginLeft: '-2.5rem'
+    backgroundColor: theme?.colors?.white
 }));
 
 
 export const TabStyle = styled.span<any>(({ theme, isSelected }) => ({
     color: theme?.colors?.pavement,
     padding: '0.5rem 0 0.625rem 0',
-    marginLeft: '2.5rem',
     cursor: 'pointer',
+    '& + span': {
+        marginLeft: '2.5rem',
+    },
     ...(isSelected) && {
         color: theme?.colors?.cinder,
         paddingBottom: '0.5rem',
