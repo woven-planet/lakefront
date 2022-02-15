b056b50a1f1ea8ac346439af2545b17060405ead
alex-maul <alex.maul@slalom.com>
2022-02-15 16:55:08 -0500

    origin/readme/workflow
    Added automation for boilerplate code upon new components being added. Added notes in Readme for which npm script to run for this. Changed assets folder to 'screenshots' for more clarity.

---
+++ index.js
@@ -0,0 +1,42 @@
+// index.js
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+/**
+  * DATA is the object that contains all
+  * the data to be provided to Mustache
+  * Notice the "name" and "date" property.
+*/
+
+const DATA = {
+    nameOfComponent: 'NAME_OF_COMPONENT',
+    dummyURL: 'NAME_OF_COMPONENT',
+    imgSrc: 'NAME_OF_COMPONENT'
+};
+/**
+  * A - We open 'main.mustache'
+  * B - We ask Mustache to render our file with the data
+  * C - We create a README.md file with the generated output
+  */
+
+function readReadMe() {
+    if (fs.readFileSync('README.md', 'utf8').slice(-1) != '|') {
+        fs.appendFileSync('README.md', '\n');
+        generateReadMe(false);
+    } else {
+        generateReadMe(true);
+    }
+};
+
+function generateReadMe(rightColumn) {
+    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+        if (err) throw err;
+        let output = Mustache.render(data.toString(), DATA);
+        if (rightColumn) {
+            output = output.substring(0, output.length - 1);
+        }
+        fs.appendFileSync('README.md', output,);
+    });
+}
+
+readReadMe();
