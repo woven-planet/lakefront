f0efd9fc93c0a9a324f0dfad1698deecdaf480ea
alex-maul <87024084+alex-maul@users.noreply.github.com>
2022-07-27 11:44:39 -0400

    Snackbar style update fix (#263)
    * snackbar-style-update-fix
    
    style fix for container of snackbar
    
    * ci: version bump to 0.110.5
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

--- src/Snackbar/snackbarStyles.ts
+++ src/Snackbar/snackbarStyles.ts
@@ -7,40 +7,46 @@ import { generateAnchorOrigin, SnackbarOrigin } from './Snackbar.util';
 export const TRANSITION_CLOSE_TIME = 0;
 
 export interface SnackbarPropsStyles {
-    anchorOrigin: { horizontal: SnackbarOrigin['horizontal']; vertical: SnackbarOrigin['vertical'] },
+    anchorOrigin: { horizontal: SnackbarOrigin['horizontal']; vertical: SnackbarOrigin['vertical'] };
 }
 
 export const SnackbarWrapper = styled.div<SnackbarPropsStyles>(({ anchorOrigin }) => {
-    const portal = { style: {left: '', bottom: '', right: '', top: ''}};
+    const portal = { style: { left: '', bottom: '', right: '', top: '' } };
     // set horizontal properties
     generateAnchorOrigin(anchorOrigin, portal);
-    return {
-    '.content-snackbar-wrapper': {
-        display: 'flex',
-        alignItems: 'center',
-        marginTop: 4,
+    const snackbarStyles = {
         zIndex: theme.zIndex.snackbar,
-        position: 'fixed',
+        position: 'fixed' as const,
         left: portal.style.left || 'auto',
         top: portal.style.top || 'auto',
         right: portal.style.right || 'auto',
-        bottom: portal.style.bottom || 'auto',
-    },
-    '.snackbarOpen': {
-        minHeight: 40,
-        opacity: 1,
-        transform: `scale(${1.02}, ${1.02 ** 2})`,
-        transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, transform 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-    },
-    '.snackbarClosed': {
-        minHeight: 40,
-        opacity: 0,
-        transition: 'opacity 0ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, transform 130ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-        transform: 'none'
-    }
-};});
+        bottom: portal.style.bottom || 'auto'
+    };
 
-export const StyledSnackbarCloseButton = styled(Button) ({
+    return {
+        ...snackbarStyles,
+        '.content-snackbar-wrapper': {
+            ...snackbarStyles,
+            display: 'flex',
+            alignItems: 'center',
+            marginTop: 4
+        },
+        '.snackbarOpen': {
+            opacity: 1,
+            transform: `scale(${1.02}, ${1.02 ** 2})`,
+            transition:
+                'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, transform 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms'
+        },
+        '.snackbarClosed': {
+            opacity: 0,
+            transition:
+                'opacity 0ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, transform 130ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+            transform: 'none'
+        }
+    };
+});
+
+export const StyledSnackbarCloseButton = styled(Button)({
     alignSelf: 'center',
     transform: 'scale(0.8)'
 });
