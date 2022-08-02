edc58fb060c34f40ebc93ac8826b0d4355bf72d2
alex-maul <87024084+alex-maul@users.noreply.github.com>
2022-08-02 15:39:43 -0400

    Add-easier-multi-sort-to-table (#266)
    * Add-easier-multi-sort-to-table
    
    Updates to Table component. This will support array of initialSortBy ids and if they are desc or not. This is for sorting columns in a table based on importance of each column. Also still accepts one column value. Added test and updated syntax that was missing.
    
    * ci: version bump to 0.110.6
    
    * ci: version bump to 0.111.3
    
    * Add-easier-multi-sort-to-table
    
    comment update
    
    * ci: version bump to 0.111.4
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useTable, useSortBy, useExpanded, TableState, Column, TableSortByToggleProps } from 'react-table';
 import { StyledHeader, StyledHeaderContent, TableStyle } from './tableStyles';
 import { ThemeProvider } from '@emotion/react';
@@ -42,8 +42,10 @@ export interface TableProps {
     className?: string;
     /**
     * This is to set the initial sorting on the table.
+    * When an array of items is provided, the order dictates the priority of sorting. Example: value --> title --> percentage.
     */
-    initialSortBy?: { id: string; desc: boolean };
+    initialSortBy?: { id: string, desc: boolean}[] | { id: string, desc: boolean};
+    
     /**
      * This event is triggered when the sorting is changed on the table.
      * The first argument is the sorted column and the second argument is the sortBy array
@@ -74,81 +76,91 @@ const Table: React.FC<TableProps> = ({ className,
     initialSortBy,
     rowProps,
     renderRowSubComponent }) => {
+        /** initalSortBy must be memoized
+         * https://react-table-v7.tanstack.com/docs/api/useSortBy#table-options
+         */
+        const initialSortByData = useMemo(
+            () =>
+                initialSortBy
+                    ? { initialState: { sortBy: Array.isArray(initialSortBy) ? initialSortBy : [initialSortBy] } }
+                    : {},
+            [initialSortBy]
+        );
 
-    // Use the state and functions returned from useTable to build your UI
-    const tableHookOptions = {
-        ...options,
-        columns,
-        data,
-        ...(initialSortBy && { initialState: { sortBy: [initialSortBy] } })
-    };
-    const { getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-        state,
-        ...rest
-    } = useTable(tableHookOptions, useSortBy, useExpanded);
-    const { sortBy } = state as CustomTableOptions;
+        // Use the state and functions returned from useTable to build your UI
+        const tableHookOptions = {
+            ...options,
+            columns,
+            data,
+            ...initialSortByData
+        };
+        const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, ...rest } = useTable(
+            tableHookOptions,
+            useSortBy,
+            useExpanded
+        );
 
-    useEffect(() => {
-        if (onChangeSort && sortBy.length) {
-            onChangeSort(sortBy[0], sortBy);
-        }
-    }, [sortBy]);
+        const { sortBy } = state as CustomTableOptions;
 
-    // Render the UI for your table
-    return (
-        <ThemeProvider theme={theme}>
-            <TableStyle {...getTableProps()} className={className} style={style}>
-                <thead>
-                    {headerGroups.map((headerGroup: any) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column: any) => (
-                                <th {
-                                    ...column.getHeaderProps(column.getSortByToggleProps
-                                        (
-                                            (props: TableSortByToggleProps) =>
-                                            ({
+        useEffect(() => {
+            if (onChangeSort && sortBy.length) {
+                onChangeSort(sortBy[0], sortBy);
+            }
+        }, [sortBy]);
+
+        // Render the UI for your table
+        return (
+            <ThemeProvider theme={theme}>
+                <TableStyle {...getTableProps()} className={className} style={style}>
+                    <thead>
+                        {headerGroups.map((headerGroup: any) => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map((column: any) => (
+                                    <th
+                                        {...column.getHeaderProps(
+                                            column.getSortByToggleProps((props: TableSortByToggleProps) => ({
                                                 ...props,
-                                                title: getTitleForMultiSort(tableHookOptions.disableMultiSort, props.title, column.disableSortBy),
+                                                title: getTitleForMultiSort(
+                                                    tableHookOptions.disableMultiSort,
+                                                    props.title,
+                                                    column.disableSortBy
+                                                ),
                                                 width: column.width
-                                            })
-                                        )
-                                    )
-                                }>
-                                    <StyledHeader>
-                                        <StyledHeaderContent>{column.render('Header')}</StyledHeaderContent>
-                                        <StyledHeaderContent >
-                                            {getSortBySVG(column)}
-                                        </StyledHeaderContent>
-                                    </StyledHeader>
-                                </th>
-                            )
-                            )}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row: any) => {
-                        prepareRow(row);
-                        return (
-                            <React.Fragment key={row.id}>
-                                <tr {...row.getRowProps(rowProps)}>
-                                    {row.cells.map((cell: any) => {
-                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-                                    })}
-                                </tr>
-                                {row.isExpanded && renderRowSubComponent ? (renderRowSubComponent({ row })) : null}
-                            </React.Fragment>
-                        );
-                    })}
-                    {rows.length === 0 && <tr><td colSpan={columns.length}>{noDataMessage}</td></tr>}
-                </tbody>
-            </TableStyle>
-        </ThemeProvider>
-    );
-};
+                                            }))
+                                        )}
+                                    >
+                                        <StyledHeader>
+                                            <StyledHeaderContent>{column.render('Header')}</StyledHeaderContent>
+                                            <StyledHeaderContent>{getSortBySVG(column)}</StyledHeaderContent>
+                                        </StyledHeader>
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                        {rows.map((row: any) => {
+                            prepareRow(row);
+                            return (
+                                <React.Fragment key={row.id}>
+                                    <tr {...row.getRowProps(rowProps)}>
+                                        {row.cells.map((cell: any) => {
+                                            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                                        })}
+                                    </tr>
+                                    {row.isExpanded && renderRowSubComponent ? renderRowSubComponent({ row }) : null}
+                                </React.Fragment>
+                            );
+                        })}
+                        {rows.length === 0 && (
+                            <tr>
+                                <td colSpan={columns.length}>{noDataMessage}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </TableStyle>
+            </ThemeProvider>
+        );
+    };
 
 export default Table;
--- src/Table/__tests__/Table.test.jsx
+++ src/Table/__tests__/Table.test.jsx
@@ -34,41 +34,41 @@ const customData = [{ title: 'r2204_1_0', value: 24, percentage: 166.992, percen
 { title: 'r2010_1_0', value: 5, percentage: 25.68, percentage_change: 5.136, total: 0.1947675 },
 { title: 'r2019_1_0', value: 51, percentage: 291.549, percentage_change: 5.7166473529, total: 0.1949277202 },
 { title: 'r2020_1_0', value: 31, percentage: 271.549, percentage_change: 5.7166473529, total: 0.1749277202 },
-{ title: 'r2021_1_0', value: 41, percentage: 281.549, percentage_change: 5.7166473529, total: 0.1849277202 }]
+{ title: 'r2021_1_0', value: 41, percentage: 281.549, percentage_change: 5.7166473529, total: 0.1849277202 }];
 
 describe('<Table>', () => {
     it('check if table renders properly', () => {
         const { container } = render(<Table columns={columns} data={customData} />);
 
         expect(container.querySelectorAll('tbody tr').length).toBe(6);
-        expect(container.querySelector('table')).toHaveStyle("width: 100%;");
+        expect(container.querySelector('table')).toHaveStyle('width: 100%;');
     });
 
     it('checks if the column sorting is working', () => {
-        const mockHandleSort = jest.fn()
+        const mockHandleSort = jest.fn();
         const { container, getAllByRole } = render(<Table columns={columns} data={customData}
             initialSortBy={{ id: 'title', desc: false }} onChangeSort={mockHandleSort} />);
 
         // initial sorting applied on release column
-        expect(getAllByRole('cell')[0].textContent).toBe('r2010_1_0')
+        expect(getAllByRole('cell')[0].textContent).toBe('r2010_1_0');
 
         // sorting is not applied on percentage column
         const percentageChange = container.querySelectorAll('thead th')[3];
-        expect(getAllByRole('cell')[3].textContent).toBe('5.1360')
-        expect(getAllByRole('cell')[8].textContent).toBe('15.8140')
-        expect(getAllByRole('cell')[13].textContent).toBe('5.7166')
-        expect(getAllByRole('cell')[18].textContent).toBe('5.7166')
-        expect(getAllByRole('cell')[23].textContent).toBe('5.7166')
-        expect(getAllByRole('cell')[28].textContent).toBe('6.9580')
+        expect(getAllByRole('cell')[3].textContent).toBe('5.1360');
+        expect(getAllByRole('cell')[8].textContent).toBe('15.8140');
+        expect(getAllByRole('cell')[13].textContent).toBe('5.7166');
+        expect(getAllByRole('cell')[18].textContent).toBe('5.7166');
+        expect(getAllByRole('cell')[23].textContent).toBe('5.7166');
+        expect(getAllByRole('cell')[28].textContent).toBe('6.9580');
 
         // sorting is applied on percentage change column on click event
         fireEvent.click(percentageChange);
-        expect(getAllByRole('cell')[3].textContent).toBe('5.1360')
-        expect(getAllByRole('cell')[8].textContent).toBe('5.7166')
-        expect(getAllByRole('cell')[13].textContent).toBe('5.7166')
-        expect(getAllByRole('cell')[18].textContent).toBe('5.7166')
-        expect(getAllByRole('cell')[23].textContent).toBe('6.9580')
-        expect(getAllByRole('cell')[28].textContent).toBe('15.8140')
+        expect(getAllByRole('cell')[3].textContent).toBe('5.1360');
+        expect(getAllByRole('cell')[8].textContent).toBe('5.7166');
+        expect(getAllByRole('cell')[13].textContent).toBe('5.7166');
+        expect(getAllByRole('cell')[18].textContent).toBe('5.7166');
+        expect(getAllByRole('cell')[23].textContent).toBe('6.9580');
+        expect(getAllByRole('cell')[28].textContent).toBe('15.8140');
 
     });
 
@@ -83,11 +83,11 @@ describe('<Table>', () => {
         const { container } = render(<Table columns={columns} data={customData}
             initialSortBy={{ id: 'title', desc: false }} />);
 
-        expect(container.querySelectorAll('th')[0].title).toBe('Hold shift & click the column to add to multi-sort')
-        expect(container.querySelectorAll('th')[1].title).toBe('Hold shift & click the column to add to multi-sort')
-        expect(container.querySelectorAll('th')[2].title).toBe('Hold shift & click the column to add to multi-sort')
-        expect(container.querySelectorAll('th')[3].title).toBe('Hold shift & click the column to add to multi-sort')
-        expect(container.querySelectorAll('th')[4].title).toBe('Hold shift & click the column to add to multi-sort')
+        expect(container.querySelectorAll('th')[0].title).toBe('Hold shift & click the column to add to multi-sort');
+        expect(container.querySelectorAll('th')[1].title).toBe('Hold shift & click the column to add to multi-sort');
+        expect(container.querySelectorAll('th')[2].title).toBe('Hold shift & click the column to add to multi-sort');
+        expect(container.querySelectorAll('th')[3].title).toBe('Hold shift & click the column to add to multi-sort');
+        expect(container.querySelectorAll('th')[4].title).toBe('Hold shift & click the column to add to multi-sort');
 
     });
 
@@ -103,10 +103,18 @@ describe('<Table>', () => {
     });
 
     it('calls the mockHandleSort function', () => {
-        const mockHandleSort = jest.fn()
+        const mockHandleSort = jest.fn();
         render(<Table columns={columns} data={customData}
             initialSortBy={{ id: 'title', desc: false }} onChangeSort={mockHandleSort} />);
 
-        expect(mockHandleSort).toHaveBeenCalledWith({ "desc": false, "id": "title" }, [{ "desc": false, "id": "title" }]);
+        expect(mockHandleSort).toHaveBeenCalledWith({ 'desc': false, 'id': 'title' }, [{ 'desc': false, 'id': 'title' }]);
+    });
+
+    it('accepts array of initalSortBy data ', () => {
+        const mockHandleSort = jest.fn();
+        render(<Table columns={columns} data={customData}
+            initialSortBy={[{ id: 'value', desc: false }, { id: 'title', desc: true }, { id: 'percentage', desc: true }]} onChangeSort={mockHandleSort} />);
+
+        expect(mockHandleSort).toHaveBeenCalledWith( {'desc': false, 'id': 'value'}, [{'desc': false, 'id': 'value'}, {'desc': true, 'id': 'title'}, {'desc': true, 'id': 'percentage'}]);
     });
 });
