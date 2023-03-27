c7c551c82ea40797a2ee18e3bb8de25eb6c7b787
Amber Holland (Contractor) <amber.holland.ctr@tri.global>
2023-03-27 10:54:08 -0400

    added addon-design. status table fail

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,8 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "storybook-addon-designs"
   ],
   webpackFinal: async (config) => {
     config.resolve.modules.push(process.cwd() + "/node_modules");
