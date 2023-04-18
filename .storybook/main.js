c10655521c5f2db5d0ea590d2e280f28447a15d3
alex maul <alex.maul@slalom.com>
2023-04-18 16:25:26 -0400

    update-node-version-latest
    Upgraded node to use 18.x (18.16.0 latest) upgraded storybook to V7. (WIP) Need to configure components to render MDX / render in storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  'stories': [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  'addons': [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-mdx-gfm'
   ],
-  webpackFinal: async (config) => {
+  webpackFinal: async config => {
     config.resolve.modules.push(process.cwd() + "/node_modules");
     config.resolve.modules.push(process.cwd() + "/");
 
@@ -19,7 +20,13 @@ module.exports = {
       test: /\.svg$/,
       use: ["@svgr/webpack", "url-loader"]
     });
-
     return config;
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
