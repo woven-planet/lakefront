85fb7fa419de939bcc453eaa48942f648a664d77
alex maul <alex.maul@slalom.com>
2023-05-15 14:38:51 -0400

    export-select-overlay-styles
    Exported styles to Selects index.ts.

--- src/components/Select/index.ts
+++ src/components/Select/index.ts
@@ -1,4 +1,5 @@
 import Select, { SelectProps } from './Select';
+import { SELECT_OVERLAY_STYLES } from './selectStyles';
 
-export { SelectProps };
+export { SelectProps, SELECT_OVERLAY_STYLES };
 export default Select;
