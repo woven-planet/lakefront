3956654d79ed8bee65a8f625f59358bcb4118a7d
Jason Ransom <jason.ransom@woven-planet.global>
2025-09-11 22:34:52 -0400

    added toggle mode stories

--- src/stories/ButtonGroup/ButtonGroup.stories.tsx
+++ src/stories/ButtonGroup/ButtonGroup.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, useState, MouseEventHandler } from 'react';
 import { Meta, StoryFn } from '@storybook/react-webpack5';
 import ButtonGroupComponent, { ButtonGroupProps } from 'src/components/ButtonGroup';
 
@@ -8,29 +8,61 @@ export default {
 } as Meta;
 
 const Template: StoryFn<ButtonGroupProps & ComponentPropsWithoutRef<'div'>> = (args) => {
+  const [selected, setSelected] = useState<string>(args.selectedId);
+
+  const handleSelected: MouseEventHandler<HTMLButtonElement> = (event) => {
+    setSelected(event.currentTarget.id);
+  }
+
+  const buttonConfigs = args.buttonConfigs.map(config => ({
+    ...config,
+    onClick: selected ? handleSelected : undefined,
+  }));
+
   return (
-    <ButtonGroupComponent {...args} />
+    <ButtonGroupComponent {...args} buttonConfigs={buttonConfigs} selectedId={selected} />
   );
 };
 
 export const ButtonGroup = Template.bind({});
 ButtonGroup.args = {
   mode: 'toggle',
-  selected: undefined,
+  selectedId: 'left',
   buttonConfigs: [
-    { id: 'button1', label: 'Button 1' },
-    { id: 'button2', label: 'Button 2' },
-    { id: 'button3', label: 'Button 3' }
+    { id: 'left', label: 'Left' },
+    { id: 'right', label: 'Right' },
   ]
 };
 
 export const ThreeButtonGroup = Template.bind({});
 ThreeButtonGroup.args = {
   mode: 'toggle',
-  selected: undefined,
+  selectedId: 'middle',
   buttonConfigs: [
-    { id: 'button1', label: 'Button 1' },
-    { id: 'button2', label: 'Button 2' },
-    { id: 'button3', label: 'Button 3' }
+    { id: 'left', label: 'Left' },
+    { id: 'middle', label: 'Middle' },
+    { id: 'right', label: 'Right' }
+  ]
+};
+
+export const FourButtonGroup = Template.bind({});
+FourButtonGroup.args = {
+  mode: 'toggle',
+  selectedId: 'right-middle',
+  buttonConfigs: [
+    { id: 'left', label: 'Left' },
+    { id: 'left-middle', label: 'Middle-Left' },
+    { id: 'right-middle', label: 'Middle-Right' },
+    { id: 'right', label: 'Right' },
+  ]
+};
+
+export const NoneSelectedGroup = Template.bind({});
+NoneSelectedGroup.args = {
+  mode: 'toggle',
+  buttonConfigs: [
+    { id: 'left', label: 'Left' },
+    { id: 'middle', label: 'Middle' },
+    { id: 'right', label: 'Right' }
   ]
 };
