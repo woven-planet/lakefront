5f505b4f013341198c4dba37303abb3202f36754
Jason Ransom <jason.ransom@woven-planet.global>
2025-09-19 11:43:35 -0400

    added story for async select

--- src/stories/Select/Select.stories.tsx
+++ src/stories/Select/Select.stories.tsx
@@ -1,8 +1,9 @@
 import { ComponentPropsWithoutRef, useState } from 'react';
 import { Meta, StoryFn } from '@storybook/react-webpack5';
-import SelectComponent, { SelectProps } from 'src/components/Select';
+import SelectComponent, { SELECT_OVERLAY_STYLES, SelectProps } from 'src/components/Select';
 import DocBlock from '.storybook/DocBlock';
 import { emerald } from 'src/styles/lakefrontColors';
+import { SelectOption } from 'src/components/Select/Select';
 
 export default {
     title: 'Lakefront/Select',
@@ -45,9 +46,18 @@ const Template: StoryFn<SelectProps & ComponentPropsWithoutRef<'div'>> = (args) 
                 {areValuesSelected() && value.length > 0 && args.isMulti && `The selected values are: ${value.map(v => ` ${v}`)}`}
             </div>
             <section style={{ display: 'inline-flex', height: '150px' }}>
-                <SelectComponent options={args.options} value={value} onChange={handleOnChange} id={args.id}
-                    isSearchable={args.isSearchable} disabled={args.disabled} className={args.className}
-                    autoFocus={args.autoFocus} isMulti={args.isMulti} multiDefaultValue={args.multiDefaultValue} />
+                <SelectComponent
+                  value={value}
+                  onChange={handleOnChange}
+                  styles={{
+                    ...SELECT_OVERLAY_STYLES,
+                    control: (baseStyles, state) => ({
+                      ...SELECT_OVERLAY_STYLES.control(baseStyles, state),
+                      minWidth: 300
+                    })
+                  }}
+                  {...args}
+                />
             </section>
         </>
     );
@@ -72,4 +82,31 @@ MultiSelectWithDefaultValues.args = {
   options: [{ label: 'Km', value: 'metric' }, { label: 'Mi', value: 'imperial' }, {label: 'Made up system', value: 'made up'}],
   multiDefaultValue: [{ label: 'Mi', value: 'imperial' }, {label: 'Made up system', value: 'made up'}],
   isMulti: true
+};
+
+export const AsyncSelect = Template.bind({});
+AsyncSelect.args = {
+  options: [],
+  multiDefaultValue: [],
+  isMulti: true,
+  openMenuOnClick: false,
+  isSearchable: true,
+  asyncConfig: {
+    placeholder: 'Start typing to fetch options.',
+    noOptionsMessage: ({ inputValue }) => inputValue.length < 3 ? 'Input at least 3 characters.' : 'No matching options. Try typing "Some"',
+    loadOptions: (inputValue: string, callback: (options: SelectOption[]) => void) => {
+      if (inputValue.length < 3) {
+        callback([]);
+        return;
+      }
+      // Simulate an async call to fetch options based on the input value
+      setTimeout(() => {
+        // Here you would typically make an API call to fetch options
+        const filteredOptions = [{ label: 'Some', value: 'some' }, {label: 'Something', value: 'something'}, {label: 'Some things', value: 'some_things'}].filter((option) =>
+          option.label.toLowerCase().includes(inputValue.toLowerCase())
+        );
+        callback(filteredOptions);
+      }, 1000); // Simulate network delay
+    }
+  }
 };
